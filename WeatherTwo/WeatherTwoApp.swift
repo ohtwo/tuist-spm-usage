4c23e774bd4a3a1a7c3961ba0c7bf78f36bc78ca
Kang Byeonghak <hello@tinygram.dev>
2022-07-29 21:55:34 +0900

    Initial commit

---
+++ WeatherTwo/WeatherTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTwoApp.swift
+//  WeatherTwo
+//
+//  Created by Kang Byeonghak on 2022/07/29.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
